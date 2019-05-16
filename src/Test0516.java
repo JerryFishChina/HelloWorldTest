f6e2dc5616b8b6e772d5d9fda157efdedde32656
jerry <811635993@qq.com>
2019-05-16 16:43:00 +0800

    增加了一个类Test0516

---
+++ src/Test0516.java
@@ -0,0 +1,9 @@
+public class Test0516 {
+
+    private String testNum;
+
+    public String testOutput(String everything){
+        System.out.print("这里输出的是testOut"+everything);
+        return everything;
+    }
+}
